7928e0f2c04dfaf3b39b89623ce78a3154ab75a2
Kenneth Olukotun <kenneth.olukotun@indicina.co>
2021-10-20 15:22:58 +0100

    Initial setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from '@firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyB-zaqFXOLgDTsO7_Xmw89xzO8RJ4VcTlw',
-  authDomain: 'fir-app-932da.firebaseapp.com',
-  projectId: 'fir-app-932da',
-  storageBucket: 'fir-app-932da.appspot.com',
-  messagingSenderId: '926280741683',
-  appId: '1:926280741683:web:88b9f841857e0435bfc446',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
