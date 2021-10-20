428a7e17b4e8d952461752f4ee91c42842a082ef
Kenneth Olukotun <kenneth.olukotun@indicina.co>
2021-10-20 15:07:31 +0100

    Initial setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from '@firebase/firestore';
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyB-zaqFXOLgDTsO7_Xmw89xzO8RJ4VcTlw',
   authDomain: 'fir-app-932da.firebaseapp.com',
@@ -6,3 +10,7 @@ const firebaseConfig = {
   messagingSenderId: '926280741683',
   appId: '1:926280741683:web:88b9f841857e0435bfc446',
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
