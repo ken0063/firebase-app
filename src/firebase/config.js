4222c45ea43ae401264d53ad2315d7433a7da38f
Kenneth Olukotun <kenneth.olukotun@indicina.co>
2021-10-20 14:50:58 +0100

    Initial setup

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-zaqFXOLgDTsO7_Xmw89xzO8RJ4VcTlw',
+  authDomain: 'fir-app-932da.firebaseapp.com',
+  projectId: 'fir-app-932da',
+  storageBucket: 'fir-app-932da.appspot.com',
+  messagingSenderId: '926280741683',
+  appId: '1:926280741683:web:88b9f841857e0435bfc446',
+};
